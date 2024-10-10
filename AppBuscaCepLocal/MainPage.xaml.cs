a2033d726b1aa5f019675971fbc0df3ad83da4ca
Usuario <Usuario@CPD03-M16>
2024-10-10 15:04:24 -0300

    10/10

--- AppBuscaCepLocal/MainPage.xaml.cs
+++ AppBuscaCepLocal/MainPage.xaml.cs
@@ -1,17 +1,28 @@
-﻿namespace AppBuscaCepLocal
+﻿using AppBuscaCepLocal.Model;
+using AppBuscaCepLocal.Services;
+
+namespace AppBuscaCepLocal
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void Buscar_Clicked(object sender, EventArgs e)
+        private async void Buscar_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                Endereco? dados_endereco = await DataServiceCep.GetEnderecoByCep(txt_cep.Text);
 
+                BindingContext = dados_endereco;
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
--- AppBuscaCepLocal/Services/DataServiceCep.cs
+++ AppBuscaCepLocal/Services/DataServiceCep.cs
@@ -10,15 +10,14 @@ namespace AppBuscaCepLocal.Services
 {
     public class DataServiceCep
     {
-        public static async Task<Endereco?> GetEnderecoByCep(
-            string cep)
+        public static async Task<Endereco?> GetEnderecoByCep(string cep)
         {
             Endereco? end;
 
             using (HttpClient client = new HttpClient())
             {
                 HttpResponseMessage response = await client.GetAsync(
-                    "http://10.0.2.2:8000/endereco/by-cep?cep=" + cep);
+                    "http://localhost:8000/endereco/by-cep?cep=" + cep);
 
                 if (response.IsSuccessStatusCode)
                 {
